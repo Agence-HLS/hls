760974f90fd6acd980037a572ac25601e5758302
nobody932 <haykel93370@gmail.com>
2023-07-10 10:53:05 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,62 +1,10 @@
-import { document } from "postcss";
-import Header from "./header"
-import Main from "./main"
 import Footer from "@/components/layouts/footer";
 
 export default function Home() {
 
-  const getPerson = (firstname,lastname) => {
-      console.log(`Mon prémnom est ${firstname} et mon nom ${lastname}`)
-   
-  };
-getPerson("haykel", "alerte")
   return (
     <>
-      <Header />
-      <Main />
       <Footer />
     </>
   );
 }
-
-
-const Array =()=>{
-  let color =["green", "red", "blue"];
-console.log(color);
-color.push("black");
-console.log(color);
-color.pop();
-console.log(color);
-color.map((colore)=> {
-  console.log(`Mon élément est ${colore}`);
-  console.log(colore.slice(1, 2));
-
-  //const [...rest] = Array;
- //console.log(rest);
-
-  //let newArray = [...Array, "purple", "brown"];
- // console.log(newArray);
-
-    
-    
- 
-
-})
-
-}
- Array()
-
- let Phone={
-  //key:"value"
-  Mark:{Editor:"Apple",
-        date:"2018"},
-  os:{
-    System:"IOS",
-    Mode:"dev",
-    Date:"2023"
-  },
-  Accessories:{
-    true:"yes",
-  }
- };
-
