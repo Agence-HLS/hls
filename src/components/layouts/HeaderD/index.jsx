0258f8b1d9fabaae0aa5a28f4b456b5a2a3dea84
NeoSkyLand <lucasporet.reel@gmail.com>
2023-07-10 10:32:29 +0200

    update

--- src/components/layouts/HeaderD/index.jsx
+++ src/components/layouts/HeaderD/index.jsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 import Image from "next/image";
 import DarkMode from "@/components/tools/darkMode";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,25 +8,25 @@ const Navbar = () => {
   return (
     <header className=" bg-gray-100 dark:bg-black hidden lg:flex grid-cols-5" widt={300} height={300}>
       <div>
-        <Image src="/White-logo.svg" width={500} height={500} alt="Logo" className="dark:hidden" />
-        <Image src="/black-logo.svg" width={500} height={500} alt="Logo" className="hidden dark:flex" />
+        <Image src="/White-logo.svg" width={150} height={150} alt="Logo" className="dark:hidden" />
+        <Image src="/black-logo.svg" width={150} height={150} alt="Logo" className="hidden dark:flex" />
       </div>
       <div className="flex items-center justify-center">
-        <Image src="/pixel-man.png" width={300} height={300} alt="Image"/>
+        <Image src="/pixel-man.png" width={100} height={100} alt="Image"/>
       </div>
-      <div className=" flex  items-center justify-center">
+      <div className="w-full px-60 flex  items-center justify-center">
         <nav>
           <ul className=" text-cyan-300 dark:text-purple-500 flex flex-row text-lg">
-            <li>ACCUEIL</li>
-            <li className="px-5">NOTRE ENTREPRISE</li>
-            <li className="">NOS TARIFS</li>
-            <li className="px-5">NOS CLIENTS</li>
-            <li className="">NOUS CONTACTER</li>
+           <Link href="/">ACCUEIL</Link>
+           <Link href="/" className="px-5">NOTRE ENTREPRISE</Link>
+           <Link href="/">NOS CLIENTS</Link>
+           <Link href="/" className="px-5">NOS TARIFS</Link>
+           <Link href="/">NOUS CONTACTER</Link>
           </ul>
         </nav>
       </div>
       <div className="flex items-center justify-center">
-        <Image src="/pixel-man.png" width={300} height={300} alt="Image" className="flex items-center justify-center" />
+        <Image src="/pixel-man.png" width={100} height={100} alt="Image" className="flex items-center justify-center" />
       </div>
       <div>
         <button type="button" onClick={() => DarkMode()}>
