3dda39491c85fd01b876987efd77b6c69ef93f5e
nobody932 <haykel93370@gmail.com>
2023-07-11 09:08:55 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,24 @@
-import Footer from "@/components/layouts/footermobile/footer";
-
-export default function Home() {
-
-  return (
-    <>
-      <Footer />
-    </> )=
 "use client";
-
+import Footer from "@/components/layouts/footermobile/footer";
 import Image from "next/image";
-
 import HeaderD from "@/components/layouts/HeaderD";
 import HeaderMobile from "@/components/layouts/HeaderMobile";
 import Home from "./home";
 import { render } from "react-dom";
 export default function () {
   return (
-    <div className="">
+    <div>
+
       <HeaderD />
       <HeaderMobile />
       <Home />
+      <Footer />
+      <Image/>
+      <render/>
+
+
     </div>
 
   );
 }
+
