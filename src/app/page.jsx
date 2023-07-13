3759644b1f6a74efb010f8aa14bb0023477cb3ad
sek771 <sackos77100@gmail.com>
2023-07-13 11:06:57 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,26 +8,20 @@ import Home from "./home";
 
 export default function () {
   return (
-<<<<<<< HEAD
-    <div>
+    
 
-=======
     <>
->>>>>>> 0ebec0fee19abb1eb27e6d82c247db58cf7d0e73
       <HeaderD />
       <HeaderMobile />
       <Home />
       <Footer />
-<<<<<<< HEAD
       <Image/>
       <render/>
 
 
-    </div>
+   
 
-=======
     </>
->>>>>>> 0ebec0fee19abb1eb27e6d82c247db58cf7d0e73
   );
 }
 
