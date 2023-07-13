28d9460ce36527778aa702a8655d5f9b240ec963
nobody932 <haykel93370@gmail.com>
2023-07-13 11:25:47 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,31 +3,24 @@ import Footer from "@/components/layouts/footermobile/footer";
 import Image from "next/image";
 import HeaderD from "@/components/layouts/HeaderD";
 import HeaderMobile from "@/components/layouts/HeaderMobile";
-import Footer from "@/components/layouts/footer";
+
 import Home from "./home";
 
 export default function () {
   return (
-<<<<<<< HEAD
-    <div>
+  
 
-=======
     <>
->>>>>>> 0ebec0fee19abb1eb27e6d82c247db58cf7d0e73
       <HeaderD />
       <HeaderMobile />
       <Home />
       <Footer />
-<<<<<<< HEAD
       <Image/>
       <render/>
 
 
-    </div>
 
-=======
     </>
->>>>>>> 0ebec0fee19abb1eb27e6d82c247db58cf7d0e73
   );
 }
 
