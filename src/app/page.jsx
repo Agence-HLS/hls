3d0d7cd5f46238459d7812dacc5700acc8b6bf49
sek771 <sackos77100@gmail.com>
2023-08-28 16:02:51 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,34 +3,25 @@
 import Image from "next/image";
 import HeaderD from "@/components/layouts/HeaderD";
 import HeaderMobile from "@/components/layouts/HeaderMobile";
-<<<<<<< HEAD
 import Footer from "@/components/layouts/footermobile/footer";
-=======
 
->>>>>>> 28d9460ce36527778aa702a8655d5f9b240ec963
 import Home from "./home";
 
 export default function () {
   return (
-<<<<<<< HEAD
-=======
   
 
->>>>>>> 28d9460ce36527778aa702a8655d5f9b240ec963
     <>
       <HeaderD />
       <HeaderMobile />
       <Home />
       <Footer />
-<<<<<<< HEAD
       <Image />
-=======
       <Image/>
       <render/>
 
 
 
->>>>>>> 28d9460ce36527778aa702a8655d5f9b240ec963
     </>
   );
 }
